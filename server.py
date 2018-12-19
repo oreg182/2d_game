f2f45ab38a207307886d0389ec5099bcc50a9945
Gero <gekoepcke@gmail.com>
2018-12-19 20:09:29 +0100

    created compatibily to testmodule

--- server.py
+++ server.py
@@ -3,14 +3,23 @@ import logging as log
 import datetime
 import sys
 from threading import Thread, main_thread
+from config import get_confi
 
 PATH = sys.path[0]
 LOGPATH = PATH + '/serverlogs'
+
+RESPONSEPORT = int(get_confi("RESPONSEPORT"))
 
 timestr = datetime.datetime.now().strftime('%d-%m-%y_%H-%M-%S')
 f = open(LOGPATH + '/' + timestr + '.log', 'w')
 f.close()
 log.basicConfig(filename=LOGPATH + '/' + timestr + '.log', level=log.DEBUG)
+
+
+class Commands:
+    @staticmethod
+    def send_complete_playground():
+        pass
 
 
 class RecvConnection(Thread):
@@ -23,21 +32,24 @@ class RecvConnection(Thread):
         while main_thread().is_alive():
             sock1.listen()
             conn, addr = sock1.accept()
-            HandleConnection(conn, addr)
+            thread = HandleConnection(conn, addr)
+            thread.start()
 
 
 def send_back_to_client(addr, msg: str):
     sock = so.socket(so.AF_INET, so.SOCK_STREAM)
-    for _ in range(100):
+    for _ in range(10):
         try:
-            sock.connect(addr)
+            sock.connect((addr, RESPONSEPORT))
+            break
         except ConnectionRefusedError:
             log.info("Connection_Refused from " + str(addr))
     sock.send(msg.encode('utf-8'))
+    sock.close()
 
 
 class HandleConnection(Thread):
-    def __init__(self, conn: so.socket(), addr):
+    def __init__(self, conn: so.socket, addr):
         super(HandleConnection, self).__init__()
         self.ipaddr = addr[0]
         self.conn = conn
@@ -46,8 +58,9 @@ class HandleConnection(Thread):
     def start(self):
         msg = self.conn.recv(8192).decode('utf-8')
         print(msg)
-        if msg == '':
+        if msg == 'COMPLETE_PLAYGROUND':
             send_back_to_client(self.ipaddr, 'test')
+        self.conn.close()
 
 
 if __name__ == '__main__':
